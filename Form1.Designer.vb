852fe11babd52e777d509ee93769ddaf1dae480f
Oscar Romero <oscalito992@gmail.com>
2023-09-14 11:53:43 -0400

    version 1

---
+++ Form1.Designer.vb
@@ -0,0 +1,413 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+	Inherits System.Windows.Forms.Form
+
+	'Form overrides dispose to clean up the component list.
+	<System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+	'Required by the Windows Form Designer
+	Private components As System.ComponentModel.IContainer
+
+	'NOTE: The following procedure is required by the Windows Form Designer
+	'It can be modified using the Windows Form Designer.  
+	'Do not modify it using the code editor.
+	<System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+		Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+		Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+		Me.btnCalcular = New System.Windows.Forms.Button()
+		Me.inputNota4 = New System.Windows.Forms.TextBox()
+		Me.inputNota3 = New System.Windows.Forms.TextBox()
+		Me.inputNota2 = New System.Windows.Forms.TextBox()
+		Me.inputNota1 = New System.Windows.Forms.TextBox()
+		Me.inputNumEstudiante = New System.Windows.Forms.TextBox()
+		Me.inputApellidos = New System.Windows.Forms.TextBox()
+		Me.inputNombre = New System.Windows.Forms.TextBox()
+		Me.Label9 = New System.Windows.Forms.Label()
+		Me.Label8 = New System.Windows.Forms.Label()
+		Me.Label7 = New System.Windows.Forms.Label()
+		Me.Label6 = New System.Windows.Forms.Label()
+		Me.Label5 = New System.Windows.Forms.Label()
+		Me.Label3 = New System.Windows.Forms.Label()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.GroupBox2 = New System.Windows.Forms.GroupBox()
+		Me.lblEstado = New System.Windows.Forms.Label()
+		Me.lblNotaFinal = New System.Windows.Forms.Label()
+		Me.lblPromedio = New System.Windows.Forms.Label()
+		Me.Label11 = New System.Windows.Forms.Label()
+		Me.Label10 = New System.Windows.Forms.Label()
+		Me.Label4 = New System.Windows.Forms.Label()
+		Me.lblNumeroEstudiante = New System.Windows.Forms.Label()
+		Me.lblApellidos = New System.Windows.Forms.Label()
+		Me.lblNombreEstudiante = New System.Windows.Forms.Label()
+		Me.Label14 = New System.Windows.Forms.Label()
+		Me.Label15 = New System.Windows.Forms.Label()
+		Me.Label16 = New System.Windows.Forms.Label()
+		Me.Button1 = New System.Windows.Forms.Button()
+		Me.GroupBox1.SuspendLayout()
+		Me.GroupBox2.SuspendLayout()
+		Me.SuspendLayout()
+		'
+		'GroupBox1
+		'
+		Me.GroupBox1.BackColor = System.Drawing.Color.DarkOrange
+		Me.GroupBox1.Controls.Add(Me.btnCalcular)
+		Me.GroupBox1.Controls.Add(Me.inputNota4)
+		Me.GroupBox1.Controls.Add(Me.inputNota3)
+		Me.GroupBox1.Controls.Add(Me.inputNota2)
+		Me.GroupBox1.Controls.Add(Me.inputNota1)
+		Me.GroupBox1.Controls.Add(Me.inputNumEstudiante)
+		Me.GroupBox1.Controls.Add(Me.inputApellidos)
+		Me.GroupBox1.Controls.Add(Me.inputNombre)
+		Me.GroupBox1.Controls.Add(Me.Label9)
+		Me.GroupBox1.Controls.Add(Me.Label8)
+		Me.GroupBox1.Controls.Add(Me.Label7)
+		Me.GroupBox1.Controls.Add(Me.Label6)
+		Me.GroupBox1.Controls.Add(Me.Label5)
+		Me.GroupBox1.Controls.Add(Me.Label3)
+		Me.GroupBox1.Controls.Add(Me.Label2)
+		Me.GroupBox1.Controls.Add(Me.Label1)
+		Me.GroupBox1.Font = New System.Drawing.Font("Arial", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.GroupBox1.ForeColor = System.Drawing.SystemColors.Window
+		Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+		Me.GroupBox1.Name = "GroupBox1"
+		Me.GroupBox1.Size = New System.Drawing.Size(1000, 263)
+		Me.GroupBox1.TabIndex = 0
+		Me.GroupBox1.TabStop = False
+		Me.GroupBox1.Text = "Ingresar:"
+		'
+		'btnCalcular
+		'
+		Me.btnCalcular.ForeColor = System.Drawing.SystemColors.InactiveCaptionText
+		Me.btnCalcular.Location = New System.Drawing.Point(391, 189)
+		Me.btnCalcular.Name = "btnCalcular"
+		Me.btnCalcular.Size = New System.Drawing.Size(187, 48)
+		Me.btnCalcular.TabIndex = 16
+		Me.btnCalcular.Text = "Calcular"
+		Me.btnCalcular.UseVisualStyleBackColor = True
+		'
+		'inputNota4
+		'
+		Me.inputNota4.Location = New System.Drawing.Point(841, 100)
+		Me.inputNota4.Name = "inputNota4"
+		Me.inputNota4.Size = New System.Drawing.Size(71, 27)
+		Me.inputNota4.TabIndex = 15
+		Me.inputNota4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+		'
+		'inputNota3
+		'
+		Me.inputNota3.Location = New System.Drawing.Point(738, 99)
+		Me.inputNota3.Name = "inputNota3"
+		Me.inputNota3.Size = New System.Drawing.Size(71, 27)
+		Me.inputNota3.TabIndex = 14
+		Me.inputNota3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+		'
+		'inputNota2
+		'
+		Me.inputNota2.Location = New System.Drawing.Point(638, 98)
+		Me.inputNota2.Name = "inputNota2"
+		Me.inputNota2.Size = New System.Drawing.Size(71, 27)
+		Me.inputNota2.TabIndex = 13
+		Me.inputNota2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+		'
+		'inputNota1
+		'
+		Me.inputNota1.Location = New System.Drawing.Point(535, 99)
+		Me.inputNota1.Name = "inputNota1"
+		Me.inputNota1.Size = New System.Drawing.Size(69, 27)
+		Me.inputNota1.TabIndex = 12
+		Me.inputNota1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center
+		'
+		'inputNumEstudiante
+		'
+		Me.inputNumEstudiante.Location = New System.Drawing.Point(254, 125)
+		Me.inputNumEstudiante.Name = "inputNumEstudiante"
+		Me.inputNumEstudiante.Size = New System.Drawing.Size(178, 27)
+		Me.inputNumEstudiante.TabIndex = 11
+		'
+		'inputApellidos
+		'
+		Me.inputApellidos.Location = New System.Drawing.Point(254, 84)
+		Me.inputApellidos.Name = "inputApellidos"
+		Me.inputApellidos.Size = New System.Drawing.Size(178, 27)
+		Me.inputApellidos.TabIndex = 10
+		'
+		'inputNombre
+		'
+		Me.inputNombre.Location = New System.Drawing.Point(254, 46)
+		Me.inputNombre.Name = "inputNombre"
+		Me.inputNombre.Size = New System.Drawing.Size(178, 27)
+		Me.inputNombre.TabIndex = 9
+		'
+		'Label9
+		'
+		Me.Label9.AutoSize = True
+		Me.Label9.Location = New System.Drawing.Point(840, 65)
+		Me.Label9.Name = "Label9"
+		Me.Label9.Size = New System.Drawing.Size(65, 19)
+		Me.Label9.TabIndex = 8
+		Me.Label9.Text = "Nota #4"
+		'
+		'Label8
+		'
+		Me.Label8.AutoSize = True
+		Me.Label8.Location = New System.Drawing.Point(736, 65)
+		Me.Label8.Name = "Label8"
+		Me.Label8.Size = New System.Drawing.Size(65, 19)
+		Me.Label8.TabIndex = 7
+		Me.Label8.Text = "Nota #3"
+		'
+		'Label7
+		'
+		Me.Label7.AutoSize = True
+		Me.Label7.Location = New System.Drawing.Point(637, 65)
+		Me.Label7.Name = "Label7"
+		Me.Label7.Size = New System.Drawing.Size(65, 19)
+		Me.Label7.TabIndex = 6
+		Me.Label7.Text = "Nota #2"
+		'
+		'Label6
+		'
+		Me.Label6.AutoSize = True
+		Me.Label6.Location = New System.Drawing.Point(532, 65)
+		Me.Label6.Name = "Label6"
+		Me.Label6.Size = New System.Drawing.Size(65, 19)
+		Me.Label6.TabIndex = 5
+		Me.Label6.Text = "Nota #1"
+		'
+		'Label5
+		'
+		Me.Label5.AutoSize = True
+		Me.Label5.Location = New System.Drawing.Point(682, 23)
+		Me.Label5.Name = "Label5"
+		Me.Label5.Size = New System.Drawing.Size(50, 19)
+		Me.Label5.TabIndex = 4
+		Me.Label5.Text = "Notas" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10)
+		Me.Label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		'
+		'Label3
+		'
+		Me.Label3.AutoSize = True
+		Me.Label3.Location = New System.Drawing.Point(6, 131)
+		Me.Label3.Name = "Label3"
+		Me.Label3.Size = New System.Drawing.Size(175, 19)
+		Me.Label3.TabIndex = 2
+		Me.Label3.Text = "Número de Estudiante:"
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.Location = New System.Drawing.Point(6, 87)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(186, 19)
+		Me.Label2.TabIndex = 1
+		Me.Label2.Text = "Apellidos del Estudiante:"
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Location = New System.Drawing.Point(6, 49)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(178, 19)
+		Me.Label1.TabIndex = 0
+		Me.Label1.Text = "Nombre del Estudiante:"
+		'
+		'GroupBox2
+		'
+		Me.GroupBox2.Controls.Add(Me.lblEstado)
+		Me.GroupBox2.Controls.Add(Me.lblNotaFinal)
+		Me.GroupBox2.Controls.Add(Me.lblPromedio)
+		Me.GroupBox2.Controls.Add(Me.Label11)
+		Me.GroupBox2.Controls.Add(Me.Label10)
+		Me.GroupBox2.Controls.Add(Me.Label4)
+		Me.GroupBox2.Controls.Add(Me.lblNumeroEstudiante)
+		Me.GroupBox2.Controls.Add(Me.lblApellidos)
+		Me.GroupBox2.Controls.Add(Me.lblNombreEstudiante)
+		Me.GroupBox2.Controls.Add(Me.Label14)
+		Me.GroupBox2.Controls.Add(Me.Label15)
+		Me.GroupBox2.Controls.Add(Me.Label16)
+		Me.GroupBox2.Font = New System.Drawing.Font("Arial", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.GroupBox2.Location = New System.Drawing.Point(22, 291)
+		Me.GroupBox2.Name = "GroupBox2"
+		Me.GroupBox2.Size = New System.Drawing.Size(978, 200)
+		Me.GroupBox2.TabIndex = 1
+		Me.GroupBox2.TabStop = False
+		Me.GroupBox2.Text = "Resultados"
+		'
+		'lblEstado
+		'
+		Me.lblEstado.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+		Me.lblEstado.Location = New System.Drawing.Point(652, 122)
+		Me.lblEstado.Name = "lblEstado"
+		Me.lblEstado.Size = New System.Drawing.Size(271, 57)
+		Me.lblEstado.TabIndex = 24
+		Me.lblEstado.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		'
+		'lblNotaFinal
+		'
+		Me.lblNotaFinal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+		Me.lblNotaFinal.Location = New System.Drawing.Point(652, 30)
+		Me.lblNotaFinal.Name = "lblNotaFinal"
+		Me.lblNotaFinal.Size = New System.Drawing.Size(271, 28)
+		Me.lblNotaFinal.TabIndex = 23
+		Me.lblNotaFinal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		'
+		'lblPromedio
+		'
+		Me.lblPromedio.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+		Me.lblPromedio.Location = New System.Drawing.Point(654, 74)
+		Me.lblPromedio.Name = "lblPromedio"
+		Me.lblPromedio.Size = New System.Drawing.Size(269, 28)
+		Me.lblPromedio.TabIndex = 22
+		Me.lblPromedio.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+		'
+		'Label11
+		'
+		Me.Label11.AutoSize = True
+		Me.Label11.Location = New System.Drawing.Point(536, 128)
+		Me.Label11.Name = "Label11"
+		Me.Label11.Size = New System.Drawing.Size(74, 19)
+		Me.Label11.TabIndex = 21
+		Me.Label11.Text = "Mensaje:"
+		'
+		'Label10
+		'
+		Me.Label10.AutoSize = True
+		Me.Label10.Location = New System.Drawing.Point(536, 35)
+		Me.Label10.Name = "Label10"
+		Me.Label10.Size = New System.Drawing.Size(87, 19)
+		Me.Label10.TabIndex = 20
+		Me.Label10.Text = "Nota Final:"
+		'
+		'Label4
+		'
+		Me.Label4.AutoSize = True
+		Me.Label4.Location = New System.Drawing.Point(536, 79)
+		Me.Label4.Name = "Label4"
+		Me.Label4.Size = New System.Drawing.Size(84, 19)
+		Me.Label4.TabIndex = 19
+		Me.Label4.Text = "Promedio:"
+		'
+		'lblNumeroEstudiante
+		'
+		Me.lblNumeroEstudiante.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+		Me.lblNumeroEstudiante.Location = New System.Drawing.Point(254, 122)
+		Me.lblNumeroEstudiante.Name = "lblNumeroEstudiante"
+		Me.lblNumeroEstudiante.Size = New System.Drawing.Size(178, 28)
+		Me.lblNumeroEstudiante.TabIndex = 18
+		'
+		'lblApellidos
+		'
+		Me.lblApellidos.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+		Me.lblApellidos.Location = New System.Drawing.Point(254, 78)
+		Me.lblApellidos.Name = "lblApellidos"
+		Me.lblApellidos.Size = New System.Drawing.Size(178, 28)
+		Me.lblApellidos.TabIndex = 17
+		'
+		'lblNombreEstudiante
+		'
+		Me.lblNombreEstudiante.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+		Me.lblNombreEstudiante.Location = New System.Drawing.Point(254, 40)
+		Me.lblNombreEstudiante.Name = "lblNombreEstudiante"
+		Me.lblNombreEstudiante.Size = New System.Drawing.Size(178, 28)
+		Me.lblNombreEstudiante.TabIndex = 16
+		'
+		'Label14
+		'
+		Me.Label14.AutoSize = True
+		Me.Label14.Location = New System.Drawing.Point(25, 131)
+		Me.Label14.Name = "Label14"
+		Me.Label14.Size = New System.Drawing.Size(175, 19)
+		Me.Label14.TabIndex = 2
+		Me.Label14.Text = "Número de Estudiante:"
+		'
+		'Label15
+		'
+		Me.Label15.AutoSize = True
+		Me.Label15.Location = New System.Drawing.Point(25, 87)
+		Me.Label15.Name = "Label15"
+		Me.Label15.Size = New System.Drawing.Size(186, 19)
+		Me.Label15.TabIndex = 1
+		Me.Label15.Text = "Apellidos del Estudiante:"
+		'
+		'Label16
+		'
+		Me.Label16.AutoSize = True
+		Me.Label16.Location = New System.Drawing.Point(25, 49)
+		Me.Label16.Name = "Label16"
+		Me.Label16.Size = New System.Drawing.Size(178, 19)
+		Me.Label16.TabIndex = 0
+		Me.Label16.Text = "Nombre del Estudiante:"
+		'
+		'Button1
+		'
+		Me.Button1.Font = New System.Drawing.Font("Arial", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+		Me.Button1.Location = New System.Drawing.Point(403, 515)
+		Me.Button1.Name = "Button1"
+		Me.Button1.Size = New System.Drawing.Size(186, 49)
+		Me.Button1.TabIndex = 2
+		Me.Button1.Text = "Salir"
+		Me.Button1.UseVisualStyleBackColor = True
+		'
+		'Form1
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.BackColor = System.Drawing.SystemColors.ButtonFace
+		Me.ClientSize = New System.Drawing.Size(1024, 597)
+		Me.Controls.Add(Me.Button1)
+		Me.Controls.Add(Me.GroupBox2)
+		Me.Controls.Add(Me.GroupBox1)
+		Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+		Me.Name = "Form1"
+		Me.Text = "Calculadora de Notas"
+		Me.GroupBox1.ResumeLayout(False)
+		Me.GroupBox1.PerformLayout()
+		Me.GroupBox2.ResumeLayout(False)
+		Me.GroupBox2.PerformLayout()
+		Me.ResumeLayout(False)
+
+	End Sub
+
+	Friend WithEvents GroupBox1 As GroupBox
+	Friend WithEvents Label2 As Label
+	Friend WithEvents Label1 As Label
+	Friend WithEvents Label3 As Label
+	Friend WithEvents inputNumEstudiante As TextBox
+	Friend WithEvents inputApellidos As TextBox
+	Friend WithEvents inputNombre As TextBox
+	Friend WithEvents Label9 As Label
+	Friend WithEvents Label8 As Label
+	Friend WithEvents Label7 As Label
+	Friend WithEvents Label6 As Label
+	Friend WithEvents Label5 As Label
+	Friend WithEvents btnCalcular As Button
+	Friend WithEvents inputNota4 As TextBox
+	Friend WithEvents inputNota3 As TextBox
+	Friend WithEvents inputNota2 As TextBox
+	Friend WithEvents inputNota1 As TextBox
+	Friend WithEvents GroupBox2 As GroupBox
+	Friend WithEvents lblNombreEstudiante As Label
+	Friend WithEvents Label14 As Label
+	Friend WithEvents Label15 As Label
+	Friend WithEvents Label16 As Label
+	Friend WithEvents lblNotaFinal As Label
+	Friend WithEvents lblPromedio As Label
+	Friend WithEvents Label11 As Label
+	Friend WithEvents Label10 As Label
+	Friend WithEvents Label4 As Label
+	Friend WithEvents lblNumeroEstudiante As Label
+	Friend WithEvents lblApellidos As Label
+	Friend WithEvents lblEstado As Label
+	Friend WithEvents Button1 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,118 @@
+﻿'Oscar Romero Barbosa
+'Numero de estudiante: Y00592812
+'Programa calculadora de notas
+
+Public Class Form1
+	Private Sub btnIngresar_Click(sender As Object, e As EventArgs) Handles btnCalcular.Click
+
+		Const PROMEDIO As Integer = 100
+		Const CANTIDAD_NOTAS As Integer = 4
+		Dim nombreEstudiante, apellidosEstudiante, numeroEstudiante As String
+		Dim nota1, nota2, nota3, nota4, notaFinal As Double
+		Dim sumaValorNotas, promedioFinal As Double
+
+		'Validacion de entrada Nombre, Apellido y Numero de Estudiante
+		If inputNombre.Text <> "" And inputApellidos.Text <> "" And inputNumEstudiante.Text <> "" Then
+			nombreEstudiante = inputNombre.Text
+			apellidosEstudiante = inputApellidos.Text
+			numeroEstudiante = inputNumEstudiante.Text
+		Else
+			MessageBox.Show("Ingrese la informacion completa del estudiante")
+		End If
+
+		'Validacion de entrada nota #1
+		If Double.TryParse(inputNota1.Text, nota1) Then
+			If nota1 <= 100 And nota1 >= 0 Then
+				sumaValorNotas += nota1
+			Else
+				MessageBox.Show("La valor de la nota #1 no es valido!")
+			End If
+		Else
+			MessageBox.Show("La valor de la nota #1 no es valido!")
+		End If
+
+		'Validacion de entrada nota #2 
+		If Double.TryParse(inputNota2.Text, nota2) Then
+			If nota2 <= 100 And nota2 >= 0 Then
+				sumaValorNotas += nota2
+			Else
+				MessageBox.Show("La valor de la nota #2 no es valido!")
+			End If
+		Else
+			MessageBox.Show("La valor de la nota #2 no es valido!")
+		End If
+
+		'Validacion de entrada nota #3
+		If Double.TryParse(inputNota3.Text, nota3) Then
+			If nota3 <= 100 And nota3 >= 0 Then
+				sumaValorNotas += nota3
+			Else
+				MessageBox.Show("La valor de la nota #3 no es valido!")
+			End If
+		Else
+			MessageBox.Show("La valor de la nota #3 no es valido!")
+		End If
+
+		'Validacion de entrada nota #4
+		If Double.TryParse(inputNota4.Text, nota4) Then
+			If nota4 <= 100 And nota4 >= 0 Then
+				sumaValorNotas += nota4
+			Else
+				MessageBox.Show("La valor de la nota #4 no es valido!")
+			End If
+		Else
+			MessageBox.Show("La valor de la nota #4 no es valido!")
+		End If
+
+		'Colocar la informacion del estudiante en el area de resultados
+		lblNombreEstudiante.Text = inputNombre.Text
+		lblApellidos.Text = inputApellidos.Text
+		lblNumeroEstudiante.Text = inputNumEstudiante.Text
+
+		'Dividir la cantidad total de notas con la suma de los valores
+		notaFinal = sumaValorNotas / CANTIDAD_NOTAS
+
+		'Verificacion para asignar la nota
+		Select Case notaFinal
+			Case 90 To 100
+				lblNotaFinal.Text = notaFinal.ToString() + "% - A"
+			Case 80 To 89
+				lblNotaFinal.Text = notaFinal.ToString() + "% - B"
+			Case 70 To 79
+				lblNotaFinal.Text = notaFinal.ToString() + "% - C"
+			Case 60 To 69
+				lblNotaFinal.Text = notaFinal.ToString() + "% - D"
+			Case <= 59
+				lblNotaFinal.Text = notaFinal.ToString() + "% - F"
+		End Select
+
+		'Calculo para sacar el promedio final
+		promedioFinal = sumaValorNotas / PROMEDIO
+		lblPromedio.Text = promedioFinal.ToString("n")
+
+		'Mensaje de aprobacion o rechazo
+		Select Case notaFinal
+			Case >= 95
+				lblEstado.Text = "Estudiante de honor"
+			Case Else
+				lblEstado.Text = "Debe tomar un curso en verano"
+		End Select
+
+		ClearInputsText()
+
+	End Sub
+
+	Private Sub ClearInputsText()
+		inputNombre.Text = ""
+		inputApellidos.Text = ""
+		inputNumEstudiante.Text = ""
+		inputNota1.Text = ""
+		inputNota2.Text = ""
+		inputNota3.Text = ""
+		inputNota4.Text = ""
+	End Sub
+
+	Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+		Me.Close()
+	End Sub
+End Class
